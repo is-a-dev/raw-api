e6093093726974768aaeb9b4c5b0600f34fbbffa
William Harrison <william@williamdavidharrison.com.au>
2024-01-27 18:16:54 +0800

    Create update.js

---
+++ scripts/update.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+
+const directoryPath = path.join(__dirname, "../../domains");
+
+let combinedArray = [];
+
+fs.readdir(directoryPath, function (err, files) {
+    if (err) throw err;
+
+    files.forEach(function (file) {
+        const filePath = path.join(directoryPath, file);
+
+        fs.readFile(filePath, "utf8", (err, data) => {
+            if (err) throw err;
+
+            const dataArray = [JSON.parse(data)];
+
+            for (const item of dataArray) {
+                item.domain = path.parse(file).name + ".is-a.dev";
+                item.subdomain = path.parse(file).name;
+
+                item.owner.email = item.owner.email.replace(/@/, " (at) ");
+            }
+
+            combinedArray = combinedArray.concat(dataArray);
+
+            if (combinedArray.length === files.length) {
+                fs.writeFile("raw/index.json", JSON.stringify(combinedArray), (err) => {
+                    if (err) throw err;
+                });
+            }
+        });
+    });
+});
